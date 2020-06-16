317edf10222a3920bf6298c4c15061668ef29ca3
Raffian-moin <raffianmoin69@gmail.com>
2020-06-16 10:19:21 +0600

    fourth and final part

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //game values
 
 let min=1,
-	max=10,
-	winningNum=2,
+	max=7,
+	winningNum=getWinningNum(min,max),
 	guessesLeft=3;
 
 
@@ -26,16 +26,23 @@ const message=document.querySelector('.message');
 minNum.textContent=min;
 maxNum.textContent=max;
 
+game.addEventListener('mousedown',function(e){
+	if(e.target.className ==='play-again'){
+		window.location.reload();
+	}
+})
+
 
 guessBtn.addEventListener('click',function(){
 	let guess=parseInt(guessInput.value);
 
-	if(isNaN(guess) || guess < min || guess > max){
-		setMessage(`please enter a number between ${min} and ${max}`,'red')
-		console.log('wrong');
-	}
+	console.log(guess);
 
-	if(guess===winningNum){
+	if(isNaN(guess) || guess < min || guess > max){
+		guessInput.value='';
+		setMessage(`please enter a number between ${min} and ${max}`,'red');
+	}else{
+		if(guess===winningNum){
 		gameOver(true,`Your guess is correct.You win!`)
 	}else{
 		guessesLeft-=1;
@@ -48,12 +55,15 @@ guessBtn.addEventListener('click',function(){
 
 			guessInput.value='';
 
-			setMessage(`your guess is Wrong.Your guess left is ${guessesLeft}`,'red')
+			setMessage(`your guess is Wrong.Your guess left is ${guessesLeft}`,'red');
 
 
 		}
 	}
-});
+}
+	}
+
+	);
 
 function gameOver(won,msg){
 	let color;
@@ -62,6 +72,15 @@ function gameOver(won,msg){
 	guessInput.style.borderColor=color;
 
 	setMessage(msg,color);
+
+	guessBtn.value="Play again";
+
+	guessBtn.className +='play-again';
+}
+
+function getWinningNum(min,max){
+	// Math.floor(Math.random() * (max - min + 1) + min);
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function setMessage(msg,color){
